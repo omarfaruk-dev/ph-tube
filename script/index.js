8aea8ddd3da2aadc71dd911a698925ee33613b67
Omar Faruk <omarfaruk.dev@gmail.com>
2025-03-12 11:04:58 +0600

    modal added

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 //remove active class
-function removeActiveClass(){
+function removeActiveClass() {
     const activeButtons = document.getElementsByClassName("active");
-    for(let btn of activeButtons){
+    for (let btn of activeButtons) {
         btn.classList.remove("active");
     }
     console.log(activeButtons);
@@ -31,13 +31,45 @@ const loadCategoryVideos = (id) => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data =>{
+        .then(data => {
             removeActiveClass();
             const clickedButton = document.getElementById(`btn-${id}`);
             clickedButton.classList.add("active");
             displayVideos(data.category)
         });
 };
+
+//load and show video details
+const loadVideoDetails = (videoId) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displayVideoDetails(data.video)
+        })
+
+}
+const displayVideoDetails = (video) => {
+    document.getElementById('video_details').showModal()
+    const detailsContainer = document.getElementById('details-container')
+    detailsContainer.innerHTML = `
+        <div>
+           <div class="relative">
+            <img class="rounded-md w-full h-[200px] object-cover" src="${video.thumbnail}" alt="">
+            <span class="absolute bottom-3 right-4 bg-black rounded-sm text-white text-sm px-1">${video.others['posted_date']}</span>
+           </div>
+            <div class="flex items-center gap-4 mt-3">
+                <img class="w-10 h-10 rounded-full" src="${video.authors[0]['profile_picture']}" alt="">
+                <h3 class="font-bold">${video.title}</h3>
+            </div>
+            <div class="flex items-center gap-2 mx-auto pl-14">
+                
+            <small class="text-sm text-[#171717B3]">${video.description} Views</small>
+            </div>
+            
+        </div>
+        `
+}
 
 //display button categories
 function displayCategories(categories) {
@@ -53,7 +85,6 @@ function displayCategories(categories) {
         // append the element
         categoryContainer.append(categoryDiv);
     }
-
 
 }
 
@@ -73,7 +104,7 @@ const displayVideos = (videos) => {
         <div class="shadow">
                 <figure class="relative">
                  <img class="rounded-md w-full h-[200px] object-cover" src="${video.thumbnail}" alt="">
-                 <span class="absolute bottom-3 right-4 bg-black rounded-sm text-white text-sm px-1">3h 56m ago</span>
+                 <span class="absolute bottom-3 right-4 bg-black rounded-sm text-white text-sm px-1">${video.category}</span>
                 </figure>
                  <div class="flex items-center gap-4 mt-3">
                      <img class="w-10 h-10 rounded-full" src="${video.authors[0].profile_picture}" alt="">
@@ -84,7 +115,7 @@ const displayVideos = (videos) => {
                      <small class="w-5"> ${video.authors[0].verified ? `<img src="assets/verified-check.svg"` : ""}</small>
                  </div>
                  <small class="pl-14 text-sm text-[#171717B3]">${video.others.views}</small>
-                 <button class="btn btn-block mt-3">Show Details</button>
+                 <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block mt-3">Show Details</button>
              </div>
         `
         videoContainer.append(videoCard);
@@ -94,10 +125,3 @@ const displayVideos = (videos) => {
 
 //call function
 loadCategories();
-
-// [
-//     {
-//         "category_id": "1001",
-//         "category": "Music"
-//     },
-// ]
